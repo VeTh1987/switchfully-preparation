b51d4499197203adb9ee996abc3fdda93b34a21d
VeTh1987 <verheyen.Thierry@gmail.com>
2021-01-10 20:58:26 +0100

    querying the table

--- sql-khan/groceries.sql
+++ sql-khan/groceries.sql
@@ -1,8 +1,11 @@
 CREATE SCHEMA khan_groceries;
-CREATE TABLE groceries(id INTEGER, name TEXT, quantity INTEGER, PRIMARY KEY (id));
+CREATE TABLE groceries(id INTEGER, name TEXT, quantity INTEGER,aisle INTEGER, PRIMARY KEY (id));
 
-INSERT INTO groceries VALUES(1,"Bananas",4);
-INSERT INTO groceries VALUES(2,"Peanut Butter", 1);
-INSERT INTO groceries VALUES(3,"Dark chocolate bars", 2);
+INSERT INTO groceries VALUES(1,"Bananas",4,7);
+INSERT INTO groceries VALUES(2,"Peanut Butter", 1,2);
+INSERT INTO groceries VALUES(3,"Dark chocolate bars", 2,2);
+INSERT INTO groceries VALUES(4,"Ice Cream",1,12);
+INSERT INTO groceries VALUES(5,"Cherries", 6,2);
+INSERT INTO groceries VALUES(6,"Chocolate Syrup", 1,4);
 
-SELECT * FROM groceries;
+SELECT * FROM groceries WHERE aisle > 5 ORDER BY aisle;
